3857e81e7a2dbbde42e8e3a40272b5cf9e49736c
Samuel Parkinson <sam.james.parkinson@gmail.com>
2016-09-11 23:13:57 +0100

    Working app :dancer:.

--- app.py
+++ app.py
@@ -1,31 +1,74 @@
-from chalice import Chalice
+from chalice import Chalice, BadRequestError
+from datetime import datetime
+import boto3
+import json
+import logging
+import os
+import requests
+
+FIREBASE_DATABASE_ENDPOINT = 'https://gatekeeper-kew.firebaseio.com'
+FIREBASE_DATABASE_SECRET = '@@FIREBASE_DATABASE_SECRET@@'
+FIREBASE_SERVER_KEY = '@@FIREBASE_SERVER_KEY@@'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 app = Chalice(app_name='gatekeeper-event-handler')
-
+app.debug = True
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    return "OK"
 
+@app.route('/event', methods=['POST'])
+def particle_event():
+    logger.debug("Starting event request handling.")
 
-# The view function above will return {"hello": "world"}
-# whenver you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users/', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.json_body
-#     # Suppose we had some 'db' object that we used to
-#     # read/write from our database.
-#     # user_id = db.create_user(user_as_json)
-#     return {'user_id': user_id}
-#
-# See the README documentation for more examples.
-#
+    event = app.current_request.json_body
+
+    logger.info("Recieved Particle event: {}".format(json.dumps(event)))
+
+    # Check we have the required event keys.
+    for key in ['name', 'value', 'source']:
+        if key not in event:
+            raise BadRequestError("The `{}` key was missing from the event.".format(key))
+
+    # Store event in the database.
+    formatted_event = dict(event, **{
+        'published': datetime.utcnow().isoformat(' '),
+        'sort': (datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * -1
+    })
+
+    logger.info("Storing formatted event: {}".format(json.dumps(formatted_event)))
+
+    response = requests.post(
+        FIREBASE_DATABASE_ENDPOINT + '/events.json',
+        json = formatted_event,
+        params = { 'auth': FIREBASE_DATABASE_SECRET }
+    )
+
+    logger.info('Database response was: {}'.format(response))
+
+    logger.info('Sending FCM message.')
+
+    # Send FCM message.
+    response = requests.post(
+        'https://fcm.googleapis.com/fcm/send',
+        headers = {
+            'Authorization': 'key=' + FIREBASE_SERVER_KEY
+        },
+        json = {
+            'to': '/topics/' + event['name'].partition('/')[2],
+            'data': {
+                'event': {
+                    'name': event['name'],
+                    'value': event['value'],
+                    'source': event['source']
+                }
+            }
+        }
+    )
+
+    logger.info('FCM response was: {}'.format(response))
+
+    return "Thanks!"
