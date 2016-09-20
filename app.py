9d830a49d7617c0010757e018285034726d8b1d9
Samuel Parkinson <sjparkinson@users.noreply.github.com>
2016-09-20 21:48:22 +0100

    Travis CI Pipeline (#1)
    * Create .travis.yml.
    
    * :art: Cleanup requirements.txt.
    
    * Create requirements-dev.txt.
    
    * Add script deployment to .travis.yml.
    
    Use the script provider to run `chalice deploy`, requires the AWS CLI environment variables to be set.
    
    * Keep any local changes for the deploy.
    
    We're going to be replacing the placeholders with secrets from the environment.
    
    * Replace placeholders for deploying.
    
    * Add chalice dependency.
    
    * :art: flake8 suggested cleanup.
    
    * :art: More flake8 changes.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from chalice import Chalice
+from chalice import Chalice, BadRequestError
 from datetime import datetime
 import json
 import logging
@@ -12,11 +12,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 app = Chalice(app_name='gatekeeper-event-handler')
-app.debug = True
+app.debug = False
 
-@app.route('/')
-def index():
-    return "OK"
 
 @app.route('/event', methods=['POST'])
 def particle_event():
@@ -24,12 +21,12 @@ def particle_event():
 
     event = app.current_request.json_body
 
-    logger.info("Recieved Particle event: {}".format(json.dumps(event)))
+    logger.info("Recieved Particle event: {}", json.dumps(event))
 
     # Check we have the required event keys.
     for key in ['name', 'source']:
         if key not in event:
-            raise BadRequestError("The `{}` key was missing from the event.".format(key))
+            raise BadRequestError("The `{}` key was missing from the event." % key)
 
     # Store event in the database.
     formatted_event = dict(event, **{
@@ -37,26 +34,24 @@ def particle_event():
         'sort': (datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * -1
     })
 
-    logger.info("Storing formatted event: {}".format(json.dumps(formatted_event)))
+    logger.info("Storing formatted event: {}", json.dumps(formatted_event))
 
     response = requests.post(
         FIREBASE_DATABASE_ENDPOINT + '/events.json',
-        json = formatted_event,
-        params = { 'auth': FIREBASE_DATABASE_SECRET }
+        json=formatted_event,
+        params={'auth': FIREBASE_DATABASE_SECRET}
     )
 
-    logger.info('Database response was: {}'.format(response))
-    logger.info('Datebase request took: {}'.format(response.elapsed))
+    logger.info('Database response was: {} {}', response.status_code, response.reason)
+    logger.info('Datebase request took: {}', response.elapsed)
 
     logger.info('Sending FCM message.')
 
     # Send FCM message.
     response = requests.post(
         'https://fcm.googleapis.com/fcm/send',
-        headers = {
-            'Authorization': 'key=' + FIREBASE_SERVER_KEY
-        },
-        json = {
+        headers={'Authorization': 'key=' + FIREBASE_SERVER_KEY},
+        json={
             'to': '/topics/' + event['name'].partition('/')[2],
             'data': {
                 'event': {
@@ -67,7 +62,7 @@ def particle_event():
         }
     )
 
-    logger.info('FCM response was: {}'.format(response))
-    logger.info('FCM request took: {}'.format(response.elapsed))
+    logger.info('FCM response was: {} {}', response.status_code, response.reason)
+    logger.info('FCM request took: {}', response.elapsed)
 
     return "Thanks!"
