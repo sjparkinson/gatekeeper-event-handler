e18d5d80882642e07989b245fdc41b29c4a94fd8
Samuel Parkinson <sam.james.parkinson@gmail.com>
2016-09-20 21:44:51 +0100

    :art: More flake8 changes.

--- app.py
+++ app.py
@@ -12,12 +12,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 app = Chalice(app_name='gatekeeper-event-handler')
-app.debug = True
-
-
-@app.route('/')
-def index():
-    return "OK"
+app.debug = False
 
 
 @app.route('/event', methods=['POST'])
@@ -26,13 +21,12 @@ def particle_event():
 
     event = app.current_request.json_body
 
-    logger.info("Recieved Particle event: {}".format(json.dumps(event)))
+    logger.info("Recieved Particle event: {}", json.dumps(event))
 
     # Check we have the required event keys.
     for key in ['name', 'source']:
         if key not in event:
-            raise BadRequestError(
-                "The `{}` key was missing from the event.".format(key))
+            raise BadRequestError("The `{}` key was missing from the event." % key)
 
     # Store event in the database.
     formatted_event = dict(event, **{
@@ -40,26 +34,24 @@ def particle_event():
         'sort': (datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * -1
     })
 
-    logger.info("Storing formatted event: {}".format(json.dumps(formatted_event)))
+    logger.info("Storing formatted event: {}", json.dumps(formatted_event))
 
     response = requests.post(
         FIREBASE_DATABASE_ENDPOINT + '/events.json',
-        json = formatted_event,
-        params = { 'auth': FIREBASE_DATABASE_SECRET }
+        json=formatted_event,
+        params={'auth': FIREBASE_DATABASE_SECRET}
     )
 
-    logger.info('Database response was: {}'.format(response))
-    logger.info('Datebase request took: {}'.format(response.elapsed))
+    logger.info('Database response was: {} {}', response.status_code, response.reason)
+    logger.info('Datebase request took: {}', response.elapsed)
 
     logger.info('Sending FCM message.')
 
     # Send FCM message.
     response = requests.post(
         'https://fcm.googleapis.com/fcm/send',
-        headers = {
-            'Authorization': 'key=' + FIREBASE_SERVER_KEY
-        },
-        json = {
+        headers={'Authorization': 'key=' + FIREBASE_SERVER_KEY},
+        json={
             'to': '/topics/' + event['name'].partition('/')[2],
             'data': {
                 'event': {
@@ -70,7 +62,7 @@ def particle_event():
         }
     )
 
-    logger.info('FCM response was: {}'.format(response))
-    logger.info('FCM request took: {}'.format(response.elapsed))
+    logger.info('FCM response was: {} {}', response.status_code, response.reason)
+    logger.info('FCM request took: {}', response.elapsed)
 
     return "Thanks!"
