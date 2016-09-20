a14e8b6209c11658048d3485e2e5447b7bfd32d3
Samuel Parkinson <sjparkinson@users.noreply.github.com>
2016-09-20 23:39:28 +0100

    Convert string formatting to use `.format`. (#6)

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def particle_event():
     now = datetime.utcnow()
     event = app.current_request.json_body
 
-    logger.info("Recieved Particle event: {}" % json.dumps(event))
+    logger.info("Recieved Particle event: {}".format(json.dumps(event)))
 
     # Check we have the required event keys.
     for key in ['name', 'source']:
         if key not in event:
-            raise BadRequestError("The `{}` key was missing from the event." % key)
+            raise BadRequestError("The `{}` key was missing from the event.".format(key))
 
     # Store event in the database.
     formatted_event = dict(event, **{
@@ -33,7 +33,7 @@ def particle_event():
         'sort': (now - datetime(1970, 1, 1)).total_seconds() * -1
     })
 
-    logger.info("Storing formatted event: {}" % json.dumps(formatted_event))
+    logger.info("Storing formatted event: {}".format(json.dumps(formatted_event)))
 
     response = requests.post(
         FIREBASE_DATABASE_ENDPOINT + '/events.json',
@@ -41,8 +41,8 @@ def particle_event():
         params={'auth': FIREBASE_DATABASE_SECRET}
     )
 
-    logger.info('Database response was: {} {}' % response.status_code, response.reason)
-    logger.info('Datebase request took: {}' % response.elapsed)
+    logger.info('Database response was: {} {}'.format(response.status_code, response.reason))
+    logger.info('Datebase request took: {}'.format(response.elapsed))
 
     logger.info('Sending FCM message.')
 
@@ -61,7 +61,7 @@ def particle_event():
         }
     )
 
-    logger.info('FCM response was: {} {}' % response.status_code, response.reason)
-    logger.info('FCM request took: {}' % response.elapsed)
+    logger.info('FCM response was: {} {}'.format(response.status_code, response.reason))
+    logger.info('FCM request took: {}'.format(response.elapsed))
 
     return "Thanks!"
